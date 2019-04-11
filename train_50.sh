6a362a41ed666a72b6605f6d867307deb45ef139
Simonas Jocys <simonasjocys@gmail.com>
2019-04-11 13:49:47 +0300

    ...

--- train_50.sh
+++ train_50.sh
@@ -1,1 +1,1 @@
-./train.py --backbone=resnet50 --batch-size=8 --steps=2500 --epochs=25 --no-evaluation --image-min-side=224 --random-transform csv
+./train.py --backbone=resnet50 --batch-size=8 --steps=2500 --epochs=13 --no-evaluation --image-min-side=224 --random-transform csv
