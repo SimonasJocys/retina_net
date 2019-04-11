a3107cab1428e8c35aafb4af7d34172a6b02550a
Simonas Jocys <simonasjocys@gmail.com>
2019-04-11 19:14:43 +0300

    ...

--- train_101.sh
+++ train_101.sh
@@ -1,1 +1,1 @@
-./train.py --backbone=resnet101 --batch-size=8 --steps=2500 --epochs=13 --no-evaluation --image-min-side=224 --random-transform csv
+./train.py --backbone=resnet101 --batch-size=8 --steps=2500 --epochs=10 --no-evaluation --image-min-side=224 --random-transform csv
