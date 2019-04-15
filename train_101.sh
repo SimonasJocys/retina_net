d654730d479bfbc5997cf007fb6b8a31d2a83193
Simonas Jocys <simonasjocys@gmail.com>
2019-04-15 19:01:44 +0300

    ...

--- train_101.sh
+++ train_101.sh
@@ -1,1 +1,1 @@
-./train.py --snapshot='../../input/why-this-stuff-is-not-working/resnet101_csv_05.h5' --backbone=resnet101 --batch-size=8 --steps=2500 --epochs=10 --no-evaluation --image-min-side=224 --random-transform csv
+./train.py --snapshot='../input/why-this-stuff-is-not-working/resnet101_csv_05.h5' --backbone=resnet101 --batch-size=8 --steps=2500 --epochs=10 --no-evaluation --image-min-side=224 --random-transform csv
